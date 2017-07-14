fbd87842c7dff9254349a05db7466104ab31ca6f
jasonm-unity <jasonm@unity3d.com>
2017-07-14 16:37:31 -0400

    improved how "frame rate synch" retards the main thread.

--- source/FrameRecorder/Core/Engine/RecordingSession.cs
+++ source/FrameRecorder/Core/Engine/RecordingSession.cs
@@ -15,7 +15,12 @@ namespace UnityEngine.FrameRecorder
         public int m_FrameIndex; // count starts at 0.
         public double m_CurrentFrameStartTS;
         public double m_RecordingStartTS;
-        int m_InitialFrame = 0;
+
+        int     m_InitialFrame = 0;
+        float   m_FPSTimeStart;
+
+        float   m_FPSNextTimeStart;
+        int     m_FPSNextFrameCount;
 
         public RecorderSettings settings { get { return m_Recorder.settings; } }
         public bool recording { get { return m_Recorder.recording; } }
@@ -25,6 +30,8 @@ namespace UnityEngine.FrameRecorder
             if (!m_Recorder.BeginRecording(this))
                 return false;
             m_InitialFrame = Time.renderedFrameCount;
+            m_FPSTimeStart = Time.unscaledTime;
+
             m_Recorder.SignalSourcesOfStage(ERecordingSessionStage.BeginRecording, this);
             return true;
         }
@@ -52,11 +59,11 @@ namespace UnityEngine.FrameRecorder
             {
                 var frameCount = Time.renderedFrameCount - m_InitialFrame;
                 var frameLen = 1.0f / m_Recorder.settings.m_FrameRate;
-                var elapsed = Time.unscaledTime - m_RecordingStartTS;
+                var elapsed = Time.unscaledTime - m_FPSTimeStart;
                 var target = frameLen * (frameCount+1);
                 var sleep =  (int)((target - elapsed) * 1000);
 
-                if (sleep > 0)
+                if (sleep > 2)
                 {
                     if(settings.m_Verbose)
                         Debug.Log( string.Format("Recording session info => dT: {0:F1}s, Target dT: {1:F1}s, Retarding: {2}ms, fps: {3:F1}", elapsed, target, sleep, frameCount / elapsed ));
@@ -64,7 +71,22 @@ namespace UnityEngine.FrameRecorder
                 }
                 else if (sleep < -frameLen)
                     m_InitialFrame--;
+                else
+                    Debug.Log( string.Format("Recording session info => fps: {0:F1}", frameCount / elapsed ));
+
+                // reset every 30 frames
+                if (frameCount % 50 == 49)
+                {
+                    m_FPSNextTimeStart = Time.unscaledTime;
+                    m_FPSNextFrameCount = Time.renderedFrameCount;
+                }
+                if (frameCount % 100 == 99)
+                {
+                    m_FPSTimeStart = m_FPSNextTimeStart;
+                    m_InitialFrame = m_FPSNextFrameCount;
+                }
             }
+
         }
 
         public void PrepareNewFrame()
