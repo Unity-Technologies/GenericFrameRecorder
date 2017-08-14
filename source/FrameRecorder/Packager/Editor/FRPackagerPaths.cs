5c98790fa516fe7e24174bfa328d4247c03bc85c
jasonm-unity <jasonm@unity3d.com>
2017-08-14 15:09:17 -0400

    moved audio input out to external project.

--- source/FrameRecorder/Packager/Editor/FRPackagerPaths.cs
+++ source/FrameRecorder/Packager/Editor/FRPackagerPaths.cs
@@ -7,24 +7,12 @@ namespace UnityEditor.FrameRecorder
     {
         public static string GetFrameRecorderRootPath()
         {
-            ScriptableObject dummy = ScriptableObject.CreateInstance<FRPackagerPaths>();
+            var dummy = ScriptableObject.CreateInstance<FRPackagerPaths>();
             string path = Application.dataPath + AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(dummy)).Substring("Assets".Length);
-
+            
             path = path.Substring(path.IndexOf("Assets"));
-            path = path.Substring(0, path.LastIndexOf('/'));
-            path = path.Substring(0, path.LastIndexOf('/'));
-            path = path.Substring(0, path.LastIndexOf('/'));
+            path = path.Replace("/Framework/FrameRecorder/Packager/Editor/FRPackagerPaths.cs", "");
             return path;
-        }
-
-        public static string GetIntegrationPath()
-        {
-            return Path.Combine(GetFrameRecorderRootPath(), "Integrations");
-        }
-
-        public static string GetIntegrationPackagePath()
-        {
-            return Path.Combine(GetIntegrationPath(), "SelfExtractPackages");
         }
     }
 }
--- source/FrameRecorder/Packager/Private/Editor/FRPackager.cs
+++ source/FrameRecorder/Packager/Private/Editor/FRPackager.cs
@@ -5,22 +5,47 @@ namespace UnityEditor.FrameRecorder
 {
     static class FRPackager
     {
-        const string k_PackageName = "GenericFrameRecorder";
+        const string k_PackageName = "Recorder";
+        
+        public static string GetFrameRecorderRootPath()
+        {
+            return Application.dataPath + "/Recorder/";
+        }
 
-        [MenuItem("Assets/FrameRecorder - Generate Package")]
+        [MenuItem("Assets/Recorder/Generate Framework Package")]
         static void GeneratePackage()
         {
             var rootPath = FRPackagerPaths.GetFrameRecorderRootPath();
 
             string[] files = new string[]
             {
-                Path.Combine(rootPath, "Core" ),
-                Path.Combine(rootPath, "Inputs" ),
-                Path.Combine(rootPath, "Recorders" ),
-                Path.Combine(rootPath, "Packager/Editor" ),
-                Path.Combine(rootPath, "Integrations/FrameCapturer/Editor" ), FRPackagerPaths.GetIntegrationPackagePath(),
+                Path.Combine(rootPath, "Framework/Core" ),
+                Path.Combine(rootPath, "Framework/Recorders" ),
+                Path.Combine(rootPath, "Framework/Packager/Editor" ),
             };
             var destFile = k_PackageName + ".unitypackage";
+            AssetDatabase.ExportPackage(files, destFile, ExportPackageOptions.Recurse);
+            Debug.Log("Generated package: " + destFile);
+        }
+
+        [MenuItem("Assets/Recorder/Generate Package (full)")]
+        static void GeneratePackageFull()
+        {
+            var rootPath = FRPackagerPaths.GetFrameRecorderRootPath();
+
+            MovieRecorderPackager.GeneratePackage();
+            AssetDatabase.Refresh();
+
+            var files = new []
+            {
+                Path.Combine(rootPath, "Framework/Core" ),
+                Path.Combine(rootPath, "Framework/Recorders" ),
+                Path.Combine(rootPath, "Framework/Packager/Editor" ),
+                Path.Combine(rootPath, "Extensions/UTJ" ),
+                Path.Combine(rootPath, "Extensions/FrameCapturerRecorder" ),
+                Path.Combine(rootPath, "Extensions/MovieRecorder/Packaging" ),
+            };
+            var destFile = k_PackageName + "(full).unitypackage";
             AssetDatabase.ExportPackage(files, destFile, ExportPackageOptions.Recurse);
             Debug.Log("Generated package: " + destFile);
         }
