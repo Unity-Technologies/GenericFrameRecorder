58537c9e76491a7ab3d1dfb8255cbe3c4ab7115d
jasonm-unity <jasonm@unity3d.com>
2017-09-19 16:17:43 -0400

    preview build: experimental v0.2.170919-04

--- source/FrameRecorder/Core/Engine/RecorderVersion.cs
+++ source/FrameRecorder/Core/Engine/RecorderVersion.cs
@@ -6,7 +6,7 @@ namespace UnityEditor.FrameRecorder
 {
     public class RecorderVersion : ScriptableObject
     {
-        public const string Version = "0.2.170908-09";
+        public const string Version = "0.2.170919-04";
         public const string Stage = "(experimental)";
     }
 }
