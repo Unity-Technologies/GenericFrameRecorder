99877d773395c6b3e5d98eb676fb4b2e04d40e8a
jasonm-unity <jasonm@unity3d.com>
2017-10-05 13:55:55 -0400

    0.2.0004 QA build

--- source/FrameRecorder/Core/Engine/RecorderVersion.cs
+++ source/FrameRecorder/Core/Engine/RecorderVersion.cs
@@ -8,7 +8,7 @@ namespace UnityEditor.Recorder
     public class RecorderVersion : ScriptableObject
     {
         public const string Version = "0.2";  // major.minor.build
-        public static int BuildNumber = 2;
+        public static int BuildNumber = 3;
         public static string Tag
         {
             get { return string.Format("{0}.{1:0000}", Version, BuildNumber); }
